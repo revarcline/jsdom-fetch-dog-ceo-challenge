97fdee7fcb2da51c31fd34634bfbc487ca3d6760
Alex Cline <rev.a.r.cline@gmail.com>
2021-03-19 11:02:49 -0500

    commit tree 4dd4654f007145b21c10a27e7c2f7afb12d5c7b3

--- src/index.js
+++ src/index.js
@@ -1,1 +1,36 @@
-console.log('%c HI', 'color: firebrick')
+console.log("%c HI", "color: firebrick");
+
+document.addEventListener("DOMContentLoaded", function () {
+  const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+  const dogImages = document.querySelector("div#dog-image-container");
+
+  const breedUrl = "https://dog.ceo/api/breeds/list/all";
+  const dogBreeds = document.querySelector("ul#dog-breeds");
+
+  let images = [];
+
+  function renderImages(json) {
+    images = json["message"];
+    dogImages.innerHTML = images
+      .map((url) => `<img src="${url}" class="dog-image">`)
+      .join("\n");
+  }
+
+  fetch(imgUrl)
+    .then((response) => response.json())
+    .then((json) => {
+      renderImages(json);
+    })
+    .catch((error) => {
+      throw error;
+    });
+
+  fetch(breedUrl)
+    .then((response) => response.json())
+    .then((json) => {
+      renderImages(json);
+    })
+    .catch((error) => {
+      throw error;
+    });
+});
